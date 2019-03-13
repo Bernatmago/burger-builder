b73965597cf0c97effda13bae3a0612849616c34
Bernat Martinez <bolicubo@gmail.com>
2019-03-13 12:53:48 +0100

    tons of changes

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,25 @@ import classes from './Burger.css';
 import Ingredient from './Ingredient/Ingredient';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <Ingredient key={igKey + i} type={igKey} />;
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add ingredients!</p>;
+    }
+    
+
     return (
         <div className={classes.Burger}>
             <Ingredient type='bread-top'/>
-            <Ingredient type='cheese'/>
-            <Ingredient type='meat'/>
+            {transformedIngredients}
             <Ingredient type='bread-bottom'/>
         </div>
     );
